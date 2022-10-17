b28705b6cd8764eac009611c79852d15132fd328
Farzan Khan <101061826+FarzanKhan202409@users.noreply.github.com>
2022-10-17 23:21:49 +0530

    Create ADA Lab-4: Knapsack Problem.py

---
+++ ADA Lab-4: Knapsack Problem.py
@@ -0,0 +1,30 @@
+job=[input("ENTER JOB")]	
+job = job.split()
+deadline=[input("ENTER DEADLINE")]
+deadline = deadline.split()
+profit=[input("ENTER PROFILE")]
+profit = profit.split()
+
+job = [x in x in job]
+deadline = [int(x) in job]
+job = [int(x) in x in job]
+
+data=[(job[i],profit[i],deadline[i]) for i in range(0,len(job))]
+print(data)
+# Sort all jobs according to
+# decreasing order of profit
+
+for i in range(n):
+for j in range(n - 1 - i)
+if arr[j][2] < arr[j + 1][2]:
+   arr[j], arr[j + 1] = arr[j + 1], arr[j]
+if __name__ == '__main__':
+    arr = [['a', 2, 100],  # Job Array
+              ['b', 1, 19],
+              ['c', 2, 27],
+              ['d', 1, 25],
+              ['e', 3, 15]]
+ 
+print("Following is maximum profit sequence of jobs")            
+# Function Call
+printJobScheduling(arr, 3)
